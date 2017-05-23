a831197ff903dd70ca3ad6731919eeff6b0c1c7a
Alexander Orlov <79202875363@ya.ru>
2017-05-23 12:37:46 +0300

    Подключил Toolbar

--- CrazyHamster/app/src/main/java/ru/aleksandrorlov/crazyhamster/MainActivity.java
+++ CrazyHamster/app/src/main/java/ru/aleksandrorlov/crazyhamster/MainActivity.java
@@ -2,6 +2,9 @@ package ru.aleksandrorlov.crazyhamster;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = "MainActivity";
@@ -11,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
