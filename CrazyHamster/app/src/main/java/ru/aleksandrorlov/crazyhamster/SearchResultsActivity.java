d2204efec133b6a145501df3403820fddde5b166
Alexander Orlov <79202875363@ya.ru>
2017-06-03 15:55:57 +0300

    Шаринг полностью  реализован.

---
+++ CrazyHamster/app/src/main/java/ru/aleksandrorlov/crazyhamster/SearchResultsActivity.java
@@ -0,0 +1,10 @@
+package ru.aleksandrorlov.crazyhamster;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by alex on 03.06.17.
+ */
+
+public class SearchResultsActivity extends AppCompatActivity {
+}
